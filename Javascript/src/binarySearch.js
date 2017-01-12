e4e36be67314e73e56d0b0268b39d76c63d94819
Kent <kentmoreland@gmail.com>
2017-01-11 19:07:43 -0600

    update: added grunt and jslint

--- Javascript/src/binarySearch.js
+++ Javascript/src/binarySearch.js
@@ -5,7 +5,7 @@ const binarySearch = (array, target) => {
     mid = Math.floor((high + low) / 2);
     if(array[mid] === target){ return mid; }
     else if(target < array[mid]){ high = mid - 1; }
-    else if(target > array[mid]){ low = mid + 1; };
+    else { low = mid + 1; }
   }
     return 'Not Found';
 };
--- Javascript/test/binarySearchSpec.js
+++ Javascript/test/binarySearchSpec.js
@@ -5,17 +5,17 @@ const search = require('../src/binarySearch');
 
 describe('binarySearch', () => {
   it('should be a function', () => {
-    expect(search.binarySearch).to.be.a('Function')
-  })
+    expect(search.binarySearch).to.be.a('Function');
+  });
   it('should return an index', () => {
-    expect(search.binarySearch([1], 1)).to.be.a('number')
-  })
+    expect(search.binarySearch([1], 1)).to.be.a('number');
+  });
   it('should return the index if the item is in the array', () => {
 
-    expect(search.binarySearch([1, 2, 3, 4, 5], 3)).to.equal(2)
-    expect(search.binarySearch([1, 2, 3, 4, 5, 6], 5)).to.equal(4)
-  })
+    expect(search.binarySearch([1, 2, 3, 4, 5], 3)).to.equal(2);
+    expect(search.binarySearch([1, 2, 3, 4, 5, 6], 5)).to.equal(4);
+  });
   it('should return not found if the item is not in the array', () => {
-    expect(search.binarySearch([1, 2, 3, 4, 5], 10)).to.equal('Not Found')
-  })
-})
+    expect(search.binarySearch([1, 2, 3, 4, 5], 10)).to.equal('Not Found');
+  });
+});
